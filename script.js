d847a1b38148bc8c9e66136d2ba249a0922b081c
kosmoknot <>
2021-03-04 00:00:07 +0530

    a

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function onHover(){
   let image = document.getElementById('icon');
   let title = document.getElementById('title_content');
-  image.src = './images/explosion.gif';
+  image.src = '/images/explosion.gif';
   title.style.color =  '#ffc107';
 }
 
 function onOut(){
   let image = document.getElementById('icon');
   let title = document.getElementById('title_content');
-  image.src = './images/icon.png';
+  image.src = '/images/icon.png';
   title.style.color = 'white';
 }
