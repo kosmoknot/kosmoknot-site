5caff94a6520226a995394162fe3d7762ca2dabc
kosmoknot <>
2021-03-05 23:35:05 +0530

    included inline 3d models

---
+++ three/load_models.module.js
@@ -0,0 +1,96 @@
+import * as THREE from '/three/three.module.js';
+import { DDSLoader } from '/three/DDSLoader.js';
+import { MTLLoader } from '/three/MTLLoader.js';
+import { OBJLoader } from '/three/OBJLoader.js';
+import { OrbitControls } from '/three/OrbitControls.js';
+let camera, scene, renderer, mouse, controls;
+
+
+function load(id,path,name,cx,cy,cz,ox,oy,oz){
+  var name_obj = name+'.obj'
+  var name_mtl = name+'.mtl'
+  init(id,path,name_obj,name_mtl,cx,cy,cz,ox,oy,oz);
+  animate();
+
+}
+
+
+function init(id,path,name_obj,name_mtl,cx,cy,cz,ox,oy,oz) {
+
+  const canvas = document.querySelector(id);
+  renderer = new THREE.WebGLRenderer({canvas});
+  renderer.setPixelRatio( window.devicePixelRatio*2 );
+
+  mouse = new THREE.Vector2();
+  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+  camera.position.set(-0.8,-0.8,-0.8);
+
+
+  scene = new THREE.Scene();
+
+  const ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
+  scene.add( ambientLight );
+
+  const pointLight = new THREE.PointLight( 0xffffff, 0.8 );
+  camera.add( pointLight );
+  scene.add( camera );
+
+  controls = new OrbitControls( camera, renderer.domElement );
+  controls.listenToKeyEvents( window );
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.05;
+
+  controls.enableZoom = false;
+
+
+  const manager = new THREE.LoadingManager();
+  manager.addHandler( /\.dds$/i, new DDSLoader() );
+
+
+  new MTLLoader( manager )
+    .setPath( path )
+    .load( name_mtl, function ( materials ) {
+
+      materials.preload();
+
+      new OBJLoader( manager )
+        .setMaterials( materials )
+        .setPath( '/images/projects/' )
+        .load( name_obj, function ( object ) {
+          object.position.set(0,0,0);
+          scene.add( object );
+        }, );
+
+    } );
+
+  document.addEventListener( 'mousemove', onDocumentMouseMove );
+  window.addEventListener( 'resize', onWindowResize );
+}
+
+function onWindowResize() {
+
+  windowHalfX = window.innerWidth / 2;
+  windowHalfY = window.innerHeight / 2;
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onDocumentMouseMove( event ) {
+  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+}
+
+
+function animate() {
+
+  requestAnimationFrame( animate );
+  renderer.render( scene, camera );
+
+}
+
+export {load};
