3bd868eb5068ba024f95f4a11470aa86110b5974
kosmoknot <>
2021-03-06 17:00:09 +0530

    fixed project margins

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function onHover(){
-  let image = document.getElementById('icon');
-  let title = document.getElementById('title_content');
-  image.src = '/images/projects_explosion.gif';
-  title.style.color =  '#ffc107';
-}
 
-function onOut(){
-  let image = document.getElementById('icon');
-  let title = document.getElementById('title_content');
-  image.src = '/images/projects_icon.png';
-  title.style.color = 'white';
-}
