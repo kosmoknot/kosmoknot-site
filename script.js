2ff871546b63cc8e0f2735c69f872fce457a2064
kosmoknot <>
2021-03-09 14:12:26 +0530

    added logs

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function display(obj){
+    var content = obj.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+}
