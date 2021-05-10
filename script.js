be7f2075125ce78ae8727e7f7a7dcbb103995c6b
kosmoknot <kosmoknot@protonmail.com>
2021-05-10 16:45:40 +0530

    added search bar to logs

--- script.js
+++ script.js
@@ -1,34 +1,64 @@
-function display(obj){
-    var content = obj.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
+function display(obj) {
+  var content = obj.nextElementSibling;
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
 }
-function search(){
+function search() {
   var input = document.getElementById("searchBox");
   var filter = input.value.toUpperCase();
-  links =document.getElementsByClassName("link_dump");
-  for(var i=0; i< links.length ; i++){
-    content_url=links[i].getElementsByTagName("a")[0];
-    content_tag=links[i].getElementsByTagName("i")[0];
+  links = document.getElementsByClassName("link_dump");
+  for (var i = 0; i < links.length; i++) {
+    content_url = links[i].getElementsByTagName("a")[0];
+    content_tag = links[i].getElementsByTagName("i")[0];
     var search_url = content_url.innerHTML.toUpperCase();
     var search_tag = content_tag.innerHTML.toUpperCase();
-    if(search_url.indexOf(filter)!=-1 || search_tag.indexOf(filter)!=-1){
-      links[i].style.display="";
+    if (search_url.indexOf(filter) != -1 || search_tag.indexOf(filter) != -1) {
+      links[i].style.display = "";
+    } else {
+      links[i].style.display = "none";
+    }
+  }
+}
+
+function log_search() {
+  var input = document.getElementById("searchBox");
+  var filter = input.value.toUpperCase();
+  var visible =[];
+  var groups = document.getElementsByClassName("article");
+  names = document.getElementsByClassName("collapsible");
+  for (var i = 0; i < names.length; i++) {
+    var name = names[i].getElementsByTagName("ins")[0];
+    var search_name = name.innerHTML.toUpperCase();
+    var group = names[i].parentNode.parentNode.parentNode.parentNode;
+    var parent = names[i].parentNode;
+    if (search_name.indexOf(filter) != -1) {
+      names[i].style.display = "";
+      parent.style.display = "";
+      visible.push(group);
+    } else {
+      names[i].style.display = "none";
+      parent.style.display = "none";
+    }
+  }
+
+  for(var i=0;i<groups.length;i++){
+    if(visible.includes(groups[i])==false){
+      groups[i].style.display = "none";
     }else{
-      links[i].style.display="none";
+      groups[i].style.display = "";
     }
   }
 }
 
 function onHover() {
-  let image = document.getElementById('icon');
-  image.src = '/images/logo2.png';
+  let image = document.getElementById("icon");
+  image.src = "/images/logo2.png";
 }
 
 function onOut() {
-  let image = document.getElementById('icon');
-  image.src = '/images/logo.png';
+  let image = document.getElementById("icon");
+  image.src = "/images/logo.png";
 }
