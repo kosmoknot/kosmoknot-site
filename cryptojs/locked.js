12688d6c19dc0152c331939bbf4535a86132550d
kosmoknot <kosmoknot@protonmail.com>
2021-06-02 13:54:46 +0530

    implemented locked

--- cryptojs/locked.js
+++ cryptojs/locked.js
@@ -1,5 +1,16 @@
-function unlock(){
-    let div=document.getElementById("locked");
-    // let content;
-    // div.innerHTML=content;
+function unlock() {
+  let image = document.getElementById("blocked");
+  let passcode = document.getElementById("passcode").value;
+  let content = document.getElementById("locked_content");
+  let decrypted = CryptoJS.AES.decrypt(content.innerHTML, passcode);
+  let output = decrypted.toString(CryptoJS.enc.Utf8);    
+  let blocked =  document.getElementById("blocked");
+  if (output != "") {
+    content.innerHTML = output;
+    content.style.display = "block";
+    document.getElementById("title").innerHTML = "This page was locked";
+    blocked.style.display="none";
+} else {
+    blocked.innerHTML="Unauthorised access declined!"
+  }
 }
