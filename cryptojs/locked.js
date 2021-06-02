7f0cdceda7fc6f88f5badd634d42ea0da7fdff5d
kosmoknot <kosmoknot@protonmail.com>
2021-06-02 14:17:10 +0530

    updated locked

--- cryptojs/locked.js
+++ cryptojs/locked.js
@@ -1,16 +1,23 @@
+let count = 0; 
 function unlock() {
-  let image = document.getElementById("blocked");
   let passcode = document.getElementById("passcode").value;
   let content = document.getElementById("locked_content");
   let decrypted = CryptoJS.AES.decrypt(content.innerHTML, passcode);
   let output = decrypted.toString(CryptoJS.enc.Utf8);    
-  let blocked =  document.getElementById("blocked");
   if (output != "") {
     content.innerHTML = output;
     content.style.display = "block";
     document.getElementById("title").innerHTML = "This page was locked";
-    blocked.style.display="none";
+    document.getElementById("locked").style.display="none";
+    document.getElementById("inputs").style.display="none";
+    
 } else {
-    blocked.innerHTML="Unauthorised access declined!"
+    count++;
+    document.getElementById("locked").innerHTML+="Nope! "+(10-count)+" attempts remaining <br>"
+  }
+  if(count==10){
+    let body = document.getElementsByTagName("body")[0];
+    body.innerHTML="Nope!"
+    body.style="font-size:50px;text-align: center;"
   }
 }
